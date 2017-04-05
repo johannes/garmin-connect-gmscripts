171ec33217e08be90596dfd66b074ab63d1aba93
csthsl <steve@fusecast.com>
2017-04-05 16:21:56 -0600

    Updated protocols in the url to automatically kick off save as

--- garmin-connect-course-export.user.js
+++ garmin-connect-course-export.user.js
@@ -9,11 +9,11 @@
 
 (function() {
     var formats = [
-        {type: 'JSON', url: 'http://connect.garmin.com/proxy/course-service-1.0/json/course/',ext: '.json'},
-        {type: 'FIT', url: 'http://connect.garmin.com/proxy/course-service-1.0/fit/course/',ext: '.fit'},
-        {type: 'TCX', url: 'http://connect.garmin.com/proxy/course-service-1.0/tcx/course/',ext: '.tcx'},
-        {type: 'GPOLYLINE', url: 'http://connect.garmin.com/proxy/course-service-1.0/gpolyline/course/',ext: '.kml'},
-        {type: 'GPX', url: 'http://connect.garmin.com/proxy/course-service-1.0/gpx/course/',ext: '.gpx'}
+        {type: 'JSON', url: '//connect.garmin.com/proxy/course-service-1.0/json/course/',ext: '.json'},
+        {type: 'FIT', url: '//connect.garmin.com/proxy/course-service-1.0/fit/course/',ext: '.fit'},
+        {type: 'TCX', url: '//connect.garmin.com/proxy/course-service-1.0/tcx/course/',ext: '.tcx'},
+        {type: 'GPOLYLINE', url: '//connect.garmin.com/proxy/course-service-1.0/gpolyline/course/',ext: '.kml'},
+        {type: 'GPX', url: '//connect.garmin.com/proxy/course-service-1.0/gpx/course/',ext: '.gpx'}
     ];
 
   var id = window.location.href.replace('https://connect.garmin.com/mincourse/', '');
@@ -21,6 +21,6 @@
   for (var i=0;i<formats.length; i++) {
     ex += '<a class="export route-button-text" href="'+formats[i].url+id+formats[i].ext+'">'+formats[i].type+'</a> ';
   }
-    ex += ' <div style="display:inline-block;color:#999;padding-top:13px;font-size:11px;">(Right click + Save link as)</div></div>';
+    ex += '</div>';
   $('viewModeItems').innerHTML += ex;
 })();
